109184ed001400ed5ab9a7459c437091d7c051ef
sona angdembe <asonaxaviersm1@gmail.com>
2023-08-11 22:00:01 +0545

    this is the website isfor cat lover

---
+++ index.js
@@ -0,0 +1,12 @@
+
+let navbar = document.querySelector('.navbar');
+document.querySelector('#menu-bar').onclick=() =>{
+    navbar.classList.toggle('active');
+}
+
+
+
+  let container= document.querySelector('.container');
+  document.querySelector('#user').onclick=() =>{
+    container.classList.toggle('active');
+  }
